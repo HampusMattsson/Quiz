eb92c4f795ebb8ec790f99a7be049b0aec334ac9
HampusMattsson <44798203+HampusMattsson@users.noreply.github.com>
2019-02-05 09:37:36 +0100

    Add files via upload

--- js/skript.js
+++ js/skript.js
@@ -1,19 +1,29 @@
 var antal = 0;
 var fragor = 10;
 
+var svar = ["sten", "apa"]
+
+var frukt = ["sten", "apa"]
+var forsok = 0;
+
 function checkAnswer(){
 	var answer = document.getElementById("answer").value;
 
 	console.log(answer);
 
-	if  (answer=="sten") {
+	if (answer == frukt[forsok]){
 		console.log("du hade rätt");
 		antal=(antal+1);
-		fragor=(fragor-1);
 	} else{
-		console.log("du hade FEL")
-		fragor=(fragor-1);
+		console.log("du hade FEL");
 	}
+
+	fragor=(fragor-1);
+
+	var hejsan = document.getElementById('bild');
+	hejsan.classList.remove('bild'+forsok);
+	forsok++;
+	hejsan.classList.add('bild'+forsok);
 
 	document.getElementById("visa").innerHTML = "Dina antal rätt: "+(antal);
 	document.getElementById("kvar").innerHTML = "Antal frågor kvar: "+(fragor);
