02518cd0a6d9287c0f793db56a98820bf0dd1084
HampusMattsson <44798203+HampusMattsson@users.noreply.github.com>
2019-02-08 15:27:49 +0100

    Add files via upload

--- js/skript.js
+++ js/skript.js
@@ -1,24 +1,24 @@
 var antal = 0;
 var fragor = 10;
 
-var svar = ["sten", "apa"]
+var svar = ["playstation2", "usbc", "c++", "grafikkort", "google", "dongle", "qi", "2011", "html"]
+var froga = ["Vad heter denna konsoll?", "Vad heter denna kontakt?", "Vad heter detta programmeringsspråk?", "Vad är detta för något?", 
+"Vilka är skaparna av denna webbsida?", "Vad kallas en sån här?", "Vad heter standarden som iPhones använder för trådlös laddning?", "När dog denna herre?",
+"Vad heter detta programmeringsspråk? (har du fel får du F)"]
 
-var frukt = ["sten", "apa"]
 var forsok = 0;
 
 function checkAnswer(){
 	var answer = document.getElementById("answer").value;
 
-	console.log(answer);
+	fragor=(fragor-1);
 
-	if (answer == frukt[forsok]){
+	if (answer == svar[forsok]){
 		console.log("du hade rätt");
 		antal=(antal+1);
 	} else{
 		console.log("du hade FEL");
 	}
-
-	fragor=(fragor-1);
 
 	var hejsan = document.getElementById('bild');
 	hejsan.classList.remove('bild'+forsok);
@@ -28,6 +28,8 @@ function checkAnswer(){
 	document.getElementById("visa").innerHTML = "Dina antal rätt: "+(antal);
 	document.getElementById("kvar").innerHTML = "Antal frågor kvar: "+(fragor);
 	document.getElementById("answer").innerHTML = "";
+
+	document.getElementById("froga").innerHTML = (froga[forsok]);
 	
 	return false;
 }
